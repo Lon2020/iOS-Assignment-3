98482d73d19672e1f1de614580b7ade474fcb8a6
156514 <sorakrit.wisawapakorn@uts.edu.au>
2022-05-08 11:42:22 +1000

    Init hotel booking management system xcode project

---
+++ Hotel Booking Management System/Hotel Booking Management System/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotel Booking Management System
+//
+//  Created by วิศวปกรณ์ on 8/5/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
